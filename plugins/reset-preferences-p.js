7c00600fec14eaa3cb2dd1a7e6ca16aaf72af86a
𝓔𝓶𝓶𝓪 (𝓥𝓲𝓸𝓵𝓮𝓽'𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷) <145789380+Elpapiema@users.noreply.github.com>
2024-12-12 21:45:18 -0600

    Actualizar reset-preferences-p.js

--- plugins/reset-preferences-p.js
+++ plugins/reset-preferences-p.js
@@ -1,46 +1,28 @@
 import fs from 'fs';
 
 const filePath = './personalize.json';
-const defaultData = {
-    botName: "Alya Mikhailovna Kujou",
-    currency: "yen",
-    videos: [
-        'https://qu.ax/WgJR.mp4',
-        'https://qu.ax/kOwY.mp4',
-        'https://qu.ax/UYGf.mp4'
-    ]
-};
 
-let handler = async (m, { conn }) => {
+let handler = async (m, { isOwner }) => {
     try {
-        // Verificar si el archivo de personalización existe, si no, crearlo
-        if (!fs.existsSync(filePath)) {
-            const initialData = { default: defaultData, users: {} };
-            fs.writeFileSync(filePath, JSON.stringify(initialData, null, 2));
-        }
+        if (!fs.existsSync(filePath)) throw 'No se encontró el archivo de personalización.';
 
-        // Leer la configuración
         const config = JSON.parse(fs.readFileSync(filePath));
+        const userType = isOwner ? `owners.${m.sender}` : `users.${m.sender}`;
 
-        // Comprobar si el usuario tiene configuraciones personalizadas
-        if (!config.users[m.sender]) {
-            return conn.reply(m.chat, '❌ No tienes configuraciones personalizadas para restablecer.', m);
+        if (config[userType]) {
+            delete config[userType];
+            fs.writeFileSync(filePath, JSON.stringify(config, null, 2));
+            m.reply(`✅ Personalización eliminada para ${isOwner ? 'owner' : 'usuario'}.`);
+        } else {
+            m.reply('No hay personalización para eliminar.');
         }
-
-        // Eliminar la personalización del usuario
-        delete config.users[m.sender];
-
-        // Guardar los cambios en el archivo
-        fs.writeFileSync(filePath, JSON.stringify(config, null, 2));
-
-        conn.reply(m.chat, '✅ Todas tus preferencias han sido restablecidas a los valores predeterminados.', m);
     } catch (error) {
-        conn.reply(m.chat, `❌ Error al restablecer tus preferencias: ${error.message}`, m);
+        m.reply(`❌ Error: ${error.message}`);
     }
 };
 
 handler.help = ['resetpreferences', 'reiniciarpreferencias'];
-handler.tags = ['info'];
+handler.tags = ['owner', 'personalization'];
 handler.command = /^(resetpreferences|reiniciarpreferencias)$/i;
 
 export default handler;
