f8fbbcec1a63fcf3478aa56c9f2fc93bc5cd8e2d
𝓔𝓶𝓶𝓪 (𝓥𝓲𝓸𝓵𝓮𝓽'𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷) <145789380+Elpapiema@users.noreply.github.com>
2025-01-25 21:39:42 -0600

    Actualizar menu.js
    Actalizacion del menu

--- plugins/menu.js
+++ plugins/menu.js
@@ -51,7 +51,8 @@ let handler = async (m, { conn }) => {
   ❀ .tt .tiktok _link de tiktok_ (vídeo)
 
 ┈┈┈┈┈┈┈┈┈┈┈┈୨♡୧┈┈┈┈┈┈┈┈┈┈┈┈
-> IA 
+> IAs
+
   ❀ .bot .Alya _character AI de Alya_
 
 ┈┈┈┈┈┈┈┈┈┈┈┈୨♡୧┈┈┈┈┈┈┈┈┈┈┈┈
