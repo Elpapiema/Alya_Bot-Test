b1b50cb17747f785cadf3db11a66c619963b0b2e
𝓔𝓶𝓶𝓪 (𝓥𝓲𝓸𝓵𝓮𝓽'𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷) <145789380+Elpapiema@users.noreply.github.com>
2025-06-01 00:35:17 -0600

    patch @lid

--- handler.js
+++ handler.js
@@ -80,7 +80,13 @@ return;
         
 let _user = global.db.data && global.db.data.users && global.db.data.users[m.sender]
 
-        const isROwner = [conn.decodeJid(global.conn.user.id), ...global.owner.map(([number]) => number)].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
+        const isROwner = [
+  conn.decodeJid(global.conn.user.id),
+  ...global.owner.map(([n]) => n),
+  ...global.lidOwners.map(([n]) => n)
+]
+  .map(v => v.replace(/[^0-9]/g, ''))
+  .some(n => [`${n}@s.whatsapp.net`, `${n}@lid`].includes(m.sender))
         const isOwner = isROwner || m.fromMe
         const isMods = isOwner || global.mods.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender)
         const isPrems = isROwner || global.prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender) || _user.prem == true
