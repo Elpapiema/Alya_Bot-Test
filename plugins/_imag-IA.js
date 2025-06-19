5d0b60a866e8c0bb554d39cee20595549522134b
𝓔𝓶𝓶𝓪 (𝓥𝓲𝓸𝓵𝓮𝓽'𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷) <145789380+Elpapiema@users.noreply.github.com>
2025-06-19 09:13:56 -0600

    Actualizar _imag-IA.js

--- plugins/_imag-IA.js
+++ plugins/_imag-IA.js
@@ -2,10 +2,10 @@ let handler = async (m, { text, conn }) => {
 
 
   if (!text) {
-    return await conn.reply(m.chat, `Escribe el prompt de la imagen. Ejemplo:\n.imageia un dragón azul volando en el espacio`, m, rcanal)
+    return await conn.reply(m.chat, `Escribe el prompt de la imagen. Ejemplo:\n.imageia un dragón azul volando en el espacio`, m)
   }
 
-  await conn.reply(m.chat, `🎨 Generando imagen de: "${text}", espera un momento...`, m, rcanal)
+  await conn.reply(m.chat, `🎨 Generando imagen de: "${text}", espera un momento...`, m)
 
   try {
     let prompt = encodeURIComponent(text.trim())
